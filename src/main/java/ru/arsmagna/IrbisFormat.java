8b46c18a7e254afafb1500ebacca6e860301e20f
Alexey Mironov <amironov73@gmail.com>
2018-07-18 16:04:23 +0800

    Working on IrbisFormat class

--- src/main/java/ru/arsmagna/IrbisFormat.java
+++ src/main/java/ru/arsmagna/IrbisFormat.java
@@ -1,8 +1,13 @@
 package ru.arsmagna;
 
+import org.jetbrains.annotations.NotNull;
+
+import static ru.arsmagna.Utility.isNullOrEmpty;
+
 /**
  * Common format related stuff.
  */
+@SuppressWarnings({"unused", "WeakerAccess"})
 public class IrbisFormat {
 
     /**
@@ -26,7 +31,116 @@ public class IrbisFormat {
     public static final String INFORMATIONAL = "@info_w";
 
     /**
-     * Optimized format.
+     * Optimized (by server) format.
      */
     public static final String OPTIMIZED = "@";
+
+    //=========================================================================
+
+    /**
+     * Remove comments from the format.
+     * @param text Format to refine.
+     * @return Refined format.
+     */
+    public static String removeComments(@NotNull String text) {
+        if (isNullOrEmpty(text)) {
+            return text;
+        }
+        if (!text.contains("/*")) {
+            return text;
+        }
+
+        StringBuilder result = new StringBuilder(text.length());
+        char state = 0;
+        int index = 0, length = text.length();
+        while (index < length) {
+            char c = text.charAt(index);
+
+            switch (state) {
+                case '\'':
+                    if (c == '\'') {
+                        state = 0;
+                    }
+                    result.append(c);
+                    break;
+
+                case '"':
+                    if (c == '"') {
+                        state = 0;
+                    }
+                    result.append(c);
+                    break;
+
+                case '|':
+                    if (c == '|') {
+                        state = 0;
+                    }
+                    result.append(c);
+                    break;
+
+                default:
+                    if (c == '/') {
+                        if (index + 1 < length && text.charAt(index + 1) == '*') {
+                            while (index < length) {
+                                c = text.charAt(index);
+                                if (c == '\r' || c == '\n') {
+                                    result.append(c);
+                                    break;
+                                }
+                                index++;
+                            }
+                        }
+                        else {
+                            result.append(c);
+                        }
+                    }
+                    else if (c == '\'' || c == '"' || c == '|') {
+                        state = c;
+                        result.append(c);
+                    }
+                    else {
+                        result.append(c);
+                    }
+                    break;
+            }
+
+            index++;
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Prepare the dynamic format string.
+     * @param text Format to refine.
+     * @return Refined format.
+     */
+    public static String prepareFormat(@NotNull String text) {
+        text = removeComments(text);
+        int length = text.length();
+        if (length == 0) {
+            return text;
+        }
+
+        boolean flag = false;
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) < ' ') {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return text;
+        }
+
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            char c = text.charAt(i);
+            if (c >= ' ') {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
 }
---
+++ src/test/java/ru/arsmagna/IrbisFormatTest.java
@@ -0,0 +1,75 @@
+package ru.arsmagna;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IrbisFormatTest {
+
+    private static void testComments(String source, String expected) {
+        String actual = IrbisFormat.removeComments(source);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeComments_1() {
+        testComments("", "");
+        testComments(" ", " ");
+        testComments("v100,/,v200", "v100,/,v200");
+        testComments("\tv100\r\n", "\tv100\r\n");
+        testComments
+                (
+                        "v100/* Comment\r\nv200",
+                        "v100\r\nv200"
+                );
+        testComments
+                (
+                        "v100, '/* Not comment', v200",
+                        "v100, '/* Not comment', v200"
+                );
+        testComments
+                (
+                        "v100, |/* Not comment|, v200",
+                        "v100, |/* Not comment|, v200"
+                );
+        testComments
+                (
+                        "v100, \"/* Not comment\", v200",
+                        "v100, \"/* Not comment\", v200"
+                );
+        testComments
+                (
+                        "v100, '/*not comment' v200, /*comment\r\nv300",
+                        "v100, '/*not comment' v200, \r\nv300"
+                );
+        testComments
+                (
+                        "v100, '/*not comment' v200, /,\r\nv300",
+                        "v100, '/*not comment' v200, /,\r\nv300"
+                );
+    }
+
+    private static void testPrepare(String source, String expected) {
+        String actual = IrbisFormat.prepareFormat(source);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void prepareFormat_1() {
+        testPrepare("", "");
+        testPrepare(" ", " ");
+        testPrepare("\r\n", "");
+        testPrepare("v100,/,v200", "v100,/,v200");
+        testPrepare("\tv100\r\n", "v100");
+        testPrepare
+                (
+                        "v100/*comment\r\nv200",
+                        "v100v200"
+                );
+        testPrepare
+                (
+                        "v100 '\t'\r\nv200",
+                        "v100 ''v200"
+                );
+    }
+}
