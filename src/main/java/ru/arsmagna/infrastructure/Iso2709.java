704c18c5485dfcde72f30d2e5da74040a4fce39e
Alexey Mironov <amironov73@gmail.com>
2018-08-11 14:25:55 +0800

    Preliminary version of Iso2709.writeRecord method

--- src/main/java/ru/arsmagna/infrastructure/Iso2709.java
+++ src/main/java/ru/arsmagna/infrastructure/Iso2709.java
@@ -1,5 +1,6 @@
 package ru.arsmagna.infrastructure;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,8 +11,12 @@ import ru.arsmagna.SubField;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Iterator;
 
 /**
  * Импорт и экспорт записей в формате ISO2709.
@@ -142,4 +147,141 @@ public class Iso2709 {
         return result;
     }
 
+    private static void _Encode(char[] chars, int pos, int len, int val) {
+            len--;
+            for (pos += len; len >= 0; len--) {
+                chars[pos] = (char)(val % 10 + (byte)'0');
+                val /= 10;
+                pos--;
+            }
+    }
+
+    @Contract("_, _, null -> param2")
+    private static int _Encode(char[] chars, int pos, String str) {
+        if (str != null) {
+            for (int i = 0; i < str.length(); pos++, i++) {
+                chars[pos] = str.charAt(i);
+            }
+        }
+
+        return pos;
+    }
+
+    /**
+     * Write the record to the stream.
+     *
+     * @param stream Stream to use
+     * @param record Record to write
+     * @param encoding Encoding
+     * @throws IOException Output error
+     */
+    public static void writeRecord(@NotNull OutputStream stream,
+                                   @NotNull MarcRecord record,
+                                   @NotNull Charset encoding)
+            throws IOException
+    {
+        int recordLength = MARKER_LENGTH;
+        int dictionaryLength = 1; // С учетом ограничителя справочника
+        int[] fieldLength = new int[record.fields.size()]; // Длины полей
+
+        // Сначала подсчитываем общую длину
+        Iterator<RecordField> fields = record.fields.iterator();
+        for (int i = 0; i < record.fields.size(); i++) {
+            dictionaryLength += 12; // Одна статья справочника
+            RecordField field = fields.next();
+            int fldlen = 0;
+            if (field.tag < 10) {
+                // В фиксированном поле не бывает подполей и индикаторов
+                fldlen += field.value.length();
+            }
+            else {
+                fldlen += 2; // Индикаторы
+                if (field.value != null) {
+                    fldlen += field.value.length();
+                }
+                Iterator<SubField> subfields = field.subFields.iterator();
+                for (int j = 0; j < field.subFields.size(); j++) {
+                    SubField subfield = subfields.next();
+                    fldlen += 2; // Признак подполя и его код
+                    fldlen += subfield.value.length();
+                }
+                fldlen += 1; // Разделитель полей
+            }
+            fieldLength[i] = fldlen;
+            recordLength += fldlen;
+        }
+        recordLength += dictionaryLength; // Справочник
+        recordLength++; // Разделитель записей
+
+        // Приступаем к кодированию
+        int dictionaryPosition = MARKER_LENGTH;
+        int baseAddress = MARKER_LENGTH + dictionaryLength;
+        int currentAddress = baseAddress;
+        char[] chars = new char[recordLength]; // Закодированная запись
+
+        // Маркер записи
+        Arrays.fill(chars, ' ');
+        _Encode(chars, 0, 5, recordLength);
+        _Encode(chars, 12, 5, baseAddress);
+        chars[5] = 'n';  // Record status
+        chars[6] = 'a';  // Record type
+        chars[7] = 'm';  // Bibligraphical index
+        chars[10] = '2';
+        chars[11] = '2';
+        chars[17] = ' '; // Bibliographical level
+        chars[18] = ' '; // Cataloging rules
+        chars[19] = ' '; // Related record
+        chars[20] = '4'; // Field length
+        chars[21] = '5'; // Field offset
+        chars[22] = '0';
+        chars[23] = '0';
+
+        // Конец справочника
+        chars[baseAddress] = FIELD_DELIMITER;
+        // Проходим по полям
+        fields = record.fields.iterator();
+        for (int i = 0; i < record.fields.size(); i++) {
+            RecordField field = fields.next();
+
+            // Справочник
+            _Encode(chars, dictionaryPosition, 3, field.tag);
+            _Encode(chars, dictionaryPosition + 3, 4, fieldLength[i]);
+            _Encode(chars, dictionaryPosition + 7, 5, currentAddress - baseAddress);
+            dictionaryPosition += 12;
+
+            // Собственно поле
+            if (field.tag < 10) {
+                // В фиксированных полях не бывает подполей и индикаторов
+                currentAddress = _Encode(chars, currentAddress, field.value);
+            }
+            else {
+                // Индискаторы
+                chars[currentAddress++] = ' ';
+                chars[currentAddress++] = ' ';
+
+                // Значение поля
+                currentAddress = _Encode(chars, currentAddress, field.value);
+
+                // Подполя
+                Iterator<SubField> subfields = field.subFields.iterator();
+                for (int j = 0; j < field.subFields.size(); j++) {
+                    SubField subfield = subfields.next();
+                    chars[currentAddress++] = SUBFIELD_DELIMITER;
+                    chars[currentAddress++] = subfield.code;
+                    currentAddress = _Encode(chars, currentAddress, subfield.value);
+                }
+
+                // Ограничитель поля
+                chars[currentAddress++] = FIELD_DELIMITER;
+            }
+        }
+
+        // Конец записи
+        chars[recordLength - 1] = RECORD_DELIMITER;
+
+        // Собственно запись в поток
+        CharBuffer buffer = CharBuffer.wrap(chars);
+        ByteBuffer bytes = encoding.encode(buffer);
+        stream.write(bytes.array());
+    }
 }
--- src/test/java/ru/arsmagna/infrastructure/Iso2709Test.java
+++ src/test/java/ru/arsmagna/infrastructure/Iso2709Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import ru.arsmagna.IrbisEncoding;
 import ru.arsmagna.MarcRecord;
+import ru.arsmagna.RecordField;
 import ru.arsmagna.TestBase;
 
 import java.io.*;
@@ -12,6 +13,39 @@ import java.nio.charset.Charset;
 import static org.junit.Assert.*;
 
 public class Iso2709Test extends TestBase {
+
+    private MarcRecord getRecord() {
+        MarcRecord result = new MarcRecord();
+
+        RecordField field = new RecordField(1);
+        field.value = "ISTU/IBIS/123456790";
+        result.fields.add(field);
+
+        field = new RecordField(700);
+        field.add('a', "Иванов");
+        field.add('b', "И. И.");
+        result.fields.add(field);
+
+        field = new RecordField(701);
+        field.add('a', "Петров");
+        field.add('b', "П. П.");
+        result.fields.add(field);
+
+        field = new RecordField(200);
+        field.add('a', "Заглавие");
+        field.add('e', "подзаголовочное");
+        field.add('f', "И. И. Иванов, П. П. Петров");
+        result.fields.add(field);
+
+        field = new RecordField(300, "Первое примечание");
+        result.fields.add(field);
+        field = new RecordField(300, "Второе примечание");
+        result.fields.add(field);
+        field = new RecordField(300, "Третье примечание");
+        result.fields.add(field);
+
+        return result;
+    }
 
     @Test
     public void readRecord_1() throws IOException {
@@ -35,6 +69,16 @@ public class Iso2709Test extends TestBase {
                 count++;
             }
             assertEquals(79, count);
+        }
+    }
+
+    @Test
+    public void writeRecord_1() throws IOException {
+        Charset ansi = IrbisEncoding.ansi();
+        File file = File.createTempFile("record", ".iso");
+        try(OutputStream stream = new FileOutputStream(file)) {
+            MarcRecord record = getRecord();
+            Iso2709.writeRecord(stream, record, ansi);
         }
     }
 }
